da8065f3ee0b3cceee32700f8e6cee85c1c2c5f2
Shuning <shuning.xia@simpop.cn>
2026-05-01 07:44:35 +0800

    完成理解dataframe的取行、取列方法

--- test/c2-20260428-dataframe-loc.py
+++ test/c2-20260428-dataframe-loc.py
@@ -6,8 +6,10 @@ df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
 print(df)
 
 print("取行---------")
-print(df.loc[[0,2]])  # 取 0&2 两行
-print(df.loc[np.array([0,2])])  # 取 0&2 两行
+print("第0行:\n", df.loc[0])  # 取第 0 行
+print("第0行和第2行:\n", df.loc[[0,2]])  # 取 0&2 两行
+print("第0行和第2行:\n", df.loc[np.array([0,2])])  # 取 0&2 两行
 
 print("取列---------")
-print(df["A"])
+print("列A:\n", df["A"])  # 取单列
+print("列A和列C:\n", df[["A","C"]])    # 取多列，注意：单列返回 Series，多列返回 DataFrame
