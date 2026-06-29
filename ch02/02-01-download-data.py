349c72ab77cfa40c4998fea3c1398918a8380eaa
shuning <shnxia@126.com>
2026-06-29 09:01:34 +0800

    py version reproduction with downloading data

---
+++ ch02/02-01-download-data.py
@@ -0,0 +1,22 @@
+
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets", filter="data")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing_full = load_housing_data()
+
+# check
+if housing_full is not None:
+    print("Data loaded successfully!")
+    print(housing_full.head())
